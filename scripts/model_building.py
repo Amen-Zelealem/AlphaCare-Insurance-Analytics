72b965bb8a7517ca54e16a6ed88b1ebd89d3fd99
Amen Zelealem <amenzelealemtadese@gmail.com>
2024-12-31 17:44:24 +0300

    Import necessary libraries.

---
+++ scripts/model_building.py
@@ -0,0 +1,8 @@
+# Import necessary libraries
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score
+import shap  # Import SHAP for model interpretation
+
